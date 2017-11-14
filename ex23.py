09f31d8e6b707c748e7236443d2f2b416c1b62d3
Hassan Noueilaty <noueilaty@gmail.com>
2017-11-14 14:13:40 -0600

    completed exercise 21, and begun exercise 23

---
+++ ex23.py
@@ -0,0 +1,15 @@
+import sys
+script, encoding, error = sys.argv
+
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes
