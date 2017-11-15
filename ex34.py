874698d46219f43bb56b692c289e1f358b24564b
Hassan Noueilaty <noueilaty@gmail.com>
2017-11-15 13:51:24 -0600

    completed ex34.py

---
+++ ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+
+# 1 The animal at 1: python3.6
+# 2 The third animal: peacock
+# 3 The first animal: bear
+# 4 The animal at 3: kangaroo
+# 5 The fifth animal: whale
+# 6 The animal at 2: peacock
+# 7 The sixth animal: platypus
+# 8 The animal at 4: whale
